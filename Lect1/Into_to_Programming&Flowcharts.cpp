6157184008d6bd10f74912ffe85cb09f73555268
Pratik168960 <pratiksingh111252@gmail.com>
2026-07-07 20:09:40 +0530

    Intro to Programming & Flowcharts

---
+++ Lect1/Into_to_Programming&Flowcharts.cpp
@@ -0,0 +1,67 @@
+
+// ************** Getting Started **************
+// thought process to solve a problem 
+// 1. understand the problem 
+// 2. given values 
+// 3. try to find out approach 
+// 4. program 
+
+
+// rough solution -- flowchart or pseudocode 
+// write a program according to rough solution in some high level language called source code 
+// computer only understands 0 or 1 so we have to convert it in machine understandable format 
+// then it can be executed 
+
+// FLOWCHART --> diagramatic representation of approach
+// Components shapes 
+// oval -> terminator start or end 
+// parallelogram -> I/O 
+// rectangle -> process
+// diamond -> decision making condition is given two outpuuts only yes or no 
+// arrows -> for representation of flow 
+// circle -> connector 
+
+// sum of 2 no.
+// oval -> start 
+// parallelogram -> input a,b 
+// rectangle -> sum = a + b 
+// parallelogram -> print sum 
+// oval -> end 
+
+
+
+// PSEUDOCODE -> a way of generic representing logic 
+// syntax and way of code writing maybe different but the flow will be same in many different languages 
+// sum of 2 no. 
+// read 2 no. a & b 
+// sum = a + b 
+// print sum 
+
+// LOOPS -> iterations 
+// when we need to perform an operation multiple times we can use loops for it 
+// input n=5 print 1 to 5 i.e output : 1,2,3,4,5
+
+// flowchart 
+// oval -> start 
+// parallelogram -> read n 
+// rectangle -> let num = 1 
+// diamond -> is num <= n 
+// yes : print num and rectangle -> num = num + 1 then flow to diamond condition check
+// no :  oval -> end 
+
+
+
+// PROGRAMMING LANGUAGES
+// WHAT ? AND WHY ?
+// If i want to complete a task through computer to give instruction 
+// to the computer is only can be done with the help of some programming language 
+// each programming language has some semantics or some rules
+// as we use grammar tenses and other rules in english same as programming languages also have some syntax some rules 
+// which must be followed 
+
+// suppose for example i have a source code c++ and i directly give it to the computer 
+// will it be able to run and perform the task ?
+// the answer is NO it will not understand what you have written 
+// it will require an intrepreter(called as Compiler) which acts as an translator in between it converts the 
+// source code into binary language understandable by computer 
+// src code --> compiler --> binary language aka executable file computer can run this file 
