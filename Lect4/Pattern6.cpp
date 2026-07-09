20ce17716c0f80dfa7eefbd334b88eb803a90026
Pratik168960 <pratiksingh111252@gmail.com>
2026-07-10 01:33:48 +0530

    printing star row times upto n rows

---
+++ Lect4/Pattern6.cpp
@@ -0,0 +1,28 @@
+// output -->
+// Enter a value of n : 4
+// *
+// **
+// ***
+// ****
+
+
+// program 
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cout << "Enter a value of n : ";
+    cin >> n;
+
+    int i = 1;
+    while(i <= n){
+        int j = 1;
+        while(j <= i){
+            cout << "*";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
