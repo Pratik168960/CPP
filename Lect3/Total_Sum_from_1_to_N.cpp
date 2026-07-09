fd56513e4f02fb6abc541b4c9179efd37c5d18a3
Pratik168960 <pratiksingh111252@gmail.com>
2026-07-10 00:13:02 +0530

    Sum of numbers from 1 to n

---
+++ Lect3/Total_Sum_from_1_to_N.cpp
@@ -0,0 +1,21 @@
+//output -->
+// Enter a value for n : 5
+// Sum of numbers from 1 to 5 is 15
+
+// program
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cout << "Enter a value for n : ";
+    cin >> n;
+    int i = 0;
+    int sum = 0;
+    while(i<=n){
+        sum = sum + i;
+        i++;
+    }
+
+    cout << "Sum of numbers from 1 to " << n << " is " << sum << endl;
+}
