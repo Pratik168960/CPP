f8e70d6b8ffc02db17f21f35d1ca8652938b22f3
Pratik168960 <pratiksingh111252@gmail.com>
2026-07-10 01:49:57 +0530

    printing value starting from row number for row times upto n rows

---
+++ Lect4/Pattern9.cpp
@@ -0,0 +1,30 @@
+// output -->
+// Enter a value of n : 5
+// 1 
+// 2 3 
+// 3 4 5 
+// 4 5 6 7 
+// 5 6 7 8 9 
+
+// program 
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n ;
+    cout << "Enter a value of n : ";
+    cin >> n;
+
+    int i = 1;
+    while(i <= n){
+        int j = 1;
+        int value = i;
+        while(j <= i){
+            cout << value << " ";
+            value++;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
