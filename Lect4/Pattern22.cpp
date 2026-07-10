f5ad12be13a9a104e3a0e674e38e1fac031b53bf
Pratik168960 <pratiksingh111252@gmail.com>
2026-07-11 00:52:41 +0530

    printing star and space triangle

---
+++ Lect4/Pattern22.cpp
@@ -0,0 +1,33 @@
+// output -->
+// Enter a value of n : 5
+// *****
+//  ****
+//   ***
+//    **
+//     *
+
+// program 
+#include<iostream>
+using namespace std; 
+
+int main(){
+    int n;
+    cout << "Enter a value of n : ";
+    cin >> n;
+
+    int i = 1;
+    while (i <= n){
+        int space = i - 1;
+        while(space){
+            cout << " ";
+            space--;
+        }
+        int j = 1;
+        while (j <= n-i+1){
+            cout << "*";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
