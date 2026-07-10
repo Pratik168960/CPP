1b649f9b8ee164d61a8fe77b12cbe1d588cab420
Pratik168960 <pratiksingh111252@gmail.com>
2026-07-10 19:50:17 +0530

    Printing Alphabet for n times for n rows

---
+++ Lect4/Pattern11.cpp
@@ -0,0 +1,28 @@
+// output -->
+// Enter a value of n : 4
+// A A A A 
+// B B B B 
+// C C C C 
+// D D D D 
+
+// Program
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter a value of n : ";
+    cin >> n;
+
+    int i = 1;
+    while(i <= n){
+        int j = 1;
+        while(j <= n){
+            char ch = 'A' + i - 1;
+            cout << ch << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
