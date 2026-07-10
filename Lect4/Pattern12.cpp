72978343e02c9073a8c9ba8e12b8a39b59242e5a
Pratik168960 <pratiksingh111252@gmail.com>
2026-07-10 19:55:47 +0530

    Printing alphabet increase with column for n rows

---
+++ Lect4/Pattern12.cpp
@@ -0,0 +1,28 @@
+// output -->
+// Enter a value of n : 4
+// A B C D 
+// A B C D 
+// A B C D 
+// A B C D
+
+// Program 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter a value of n : ";
+    cin >> n; 
+
+    int i = 1 ;
+    while(i <= n){
+        int j = 1;
+        while(j <= n){
+            char ch = 'A' + j - 1;
+            cout << ch << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
