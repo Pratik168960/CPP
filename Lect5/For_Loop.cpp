cbac0319f97abf299fd1fb46940c9ae68fef32c6
Pratik168960 <pratiksingh111252@gmail.com>
2026-07-11 18:56:32 +0530

    for loop

---
+++ Lect5/For_Loop.cpp
@@ -0,0 +1,19 @@
+// ******** FOR LOOP ********
+
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cout << "Enter a value of n : ";
+    cin >> n;
+
+    cout << "printing count from 1 to n :" << endl;
+    for (int i = 1; i <= n; i++){
+        cout << i << endl;
+    }
+
+// for (initialisation; condition; updation inc/dec)
+// Initialization (once) → Condition Check → Execute Body → 
+// Update → Repeat from Condition Check (until false).
+}
